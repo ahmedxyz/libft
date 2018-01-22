f368341e2b5f609f5403d34d3018c9352b67d2d6
Hassaan Ahmed <hahmed@e1z2r7p2.42.us.org>
2018-01-21 22:56:23 -0800

    Added function ft_strndup

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hahmed <hahmed@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/19 14:37:47 by hahmed            #+#    #+#             */
-/*   Updated: 2017/10/01 11:30:19 by hahmed           ###   ########.fr       */
+/*   Updated: 2018/01/21 22:55:15 by hahmed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,5 +85,7 @@ void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+
+char				*ft_strndup(const char *s, size_t len);
 
 #endif
